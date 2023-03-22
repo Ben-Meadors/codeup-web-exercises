d6c97362b2eb03c914c2298212d4b240535c2f44
Ben Meadors <ben.p.meadors@gmail.com>
2023-03-22 15:00:19 -0400

    First CSS Doc 'Hello, World!'

--- js/circle.js
+++ js/circle.js
@@ -15,10 +15,10 @@
         logInfo: function (doRounding) {
             // TODO: complete this method.
             if (doRounding === true){
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
-            } else {
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea())
+                console.log("Rounded area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
             }
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea())
+
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
@@ -34,7 +34,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
