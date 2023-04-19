f1164ba6d3a2ffca9a1740c73bb874cbeb267e05
Ben Meadors <ben.p.meadors@gmail.com>
2023-04-19 11:03:11 -0400

    multiple selector work for jquery

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,4 +2,7 @@
 //$(function() {
 //    alert( 'The DOM has finished loading!' );
 //});
-$('.codeup').css('border', '1px solid red')
+$('li').css('font-size', '20px');
+$('h1, p, li').css('background-color', '#FF0');
+var alertH1 = $('h1').html();
+alert(alertH1);
