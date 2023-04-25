befb581a9cddb62bf66075845d64b073ef0fe62f
Ben Meadors <ben.p.meadors@gmail.com>
2023-04-25 15:02:55 -0400

    jquery faq finished, ajax store started

---
+++ js/ajax.js
@@ -0,0 +1,68 @@
+
+//--------------------------------------------
+// GOOD TO REMEMBER:
+
+// template literals:
+var mySchool = 'codeup';
+var templateLiteral = `I will graduate from ${mySchool} in 2023.`
+
+$('#container').html(`<p>this is some text, i also attend ${mySchool}.</p>`)
+
+// accessing nested items in an object:
+var cerberus = {
+    numStudents: 20,
+    location: 'remote',
+    gradYear: 2023,
+    program: 'webdev',
+    instructors: ['lola', 'doc rob', 'kenneth', 'laura'],
+    topicsCovered: {
+        html: ['elements', 'tags', 'attributes'],
+        css: ['bootstrap', 'flexbox', 'media queries']
+    }
+}
+
+var currentInstructor = cerberus.instructors[3]; // 'laura'
+
+
+// looping through arrays && looping through arrays nested in objects
+cerberus.topicsCovered.css.forEach(function(topic){
+    console.log(`I can't believe I learned about ${topic} during css`)
+})
+
+// chaining methods
+console.log($('ul').children().first().next().next().next().html()); // list item three
+
+
+// callback functions
+
+function handleUserInput(){
+    return Number(prompt("What's your favorite digit?"));
+}
+
+$('#doubleBtn').click(function(){
+    alert(handleUserInput() * 2);
+})
+
+
+
+//--------------------------------------------
+// 📌 What is AJAX
+
+// 📌 What is XML
+
+// 📌 What is JSON
+
+// 📌 What's a client
+
+// 📌 What's a server
+
+// 📌 What's a request
+
+// 📌 Types of requests
+
+// 📌 What's a response
+//--------------------------------------------
+
+
+//  📌 AJAX REQUESTS USING JQUERY
+
--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -11,3 +11,17 @@ $('dt').each(function(index) {
         $(this).toggleClass('highlight');
     })
 });
+
+$('.ol-clicker').click(function() {
+    $('ul').each(function(){
+        $(this).children().last().toggleClass('highlight');
+    })
+});
+
+$('h3').click(function() {
+    $(this).next().children().toggleClass('bold');
+});
+
+$('li').click(function() {
+    $(this).parent().children().first().toggleClass('blue');
+});
