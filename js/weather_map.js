e2641a7b94c0753a726e75f1a8fdd918545b24da
Ben Meadors <ben.p.meadors@gmail.com>
2023-05-02 05:19:51 -0400

    weathermap project updates (still missing 5day)

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,17 +1,65 @@
 'use strict'
 
-$.get("http://api.openweathermap.org/data/2.5/weather", {
-    APPID: OPEN_WEATHER_APPID,
-    q:     "Fredericksburg, US",
-    units: "imperial"
-}).done(function(data) {
-    console.log(data);
-});
-
 mapboxgl.accessToken = MAPBOX_KEY;
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
-    center: [-77.4605, 38.3032], // starting position [lng, lat]
-    zoom: 10, // starting zoom
-});
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true});
+
+function successLocation(position){
+    console.log(position);
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation(){
+    setupMap([-135, 90])
+}
+
+function setupMap(center){
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
+        center: center, // starting position [lng, lat]
+        zoom: 10, // starting zoom
+    })
+    map.scrollZoom.disable();
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+
+}
+
+
+let weather = {
+    "apiKey": OPEN_WEATHER_APPID,
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + OPEN_WEATHER_APPID).then((response) => response.json()).then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = "Weather at " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+    },
+    search: function(){
+       this.fetchWeather(document.querySelector(".search").value);
+    }
+}
+
+document.querySelector(".city-search").addEventListener("click", function (){
+    weather.search();
+})
+
+document.querySelector(".search").addEventListener("keyup", function (event){
+    if (event.key == "Enter"){
+        weather.search()
+    }
+})
+
+
+
